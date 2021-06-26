d5868be850819de813b1526f2064cf1aea27bfff
Matt Goldman <matt.goldman@outlook.com>
2021-06-26 14:27:43 +1000

    added maui project for simple chat

--- src/MauiChat.Maui/MauiChat.Maui/MainPage.xaml.cs
+++ src/MauiChat.Maui/MauiChat.Maui/MainPage.xaml.cs
@@ -1,74 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using mauichat.Shared;
-using Microsoft.AspNetCore.SignalR.Client;
+﻿using MauiChat.Maui.ViewModels;
 using Microsoft.Maui.Controls;
 
 namespace MauiChat.Maui
 {
-	public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage
 	{
-		public List<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
-
-		private HubConnection hubConnection;
-
-		private string userId;
-
-		private int avatarId;
-
-
-        public bool IsConnected =>
-			hubConnection.State == HubConnectionState.Connected;
+        public MainViewModel ViewModel{ get; set; }
 
 		public MainPage()
 		{
 			InitializeComponent();
-			_ = Initialise();
+			ViewModel = new MainViewModel();
+			BindingContext = ViewModel;
 		}
 
-		private async void OnSendClicked(object sender, EventArgs e)
-		{
-			await Send();
-		}
-
-  //      protected override void OnAppearing()
-  //      {
-		//	Initialise().Wait();
-		//}
-
-		private async Task Initialise()
+		protected override async void OnAppearing()
         {
-			userId = Guid.NewGuid().ToString();
-
-			var rnd = new Random();
-
-			avatarId = rnd.Next(1, 6);
-
-			hubConnection = new HubConnectionBuilder()
-				.WithUrl("https://maui-chat.azurewebsites.net/chathub")
-				.Build();
-
-			hubConnection.On<ChatMessage>("ReceiveMessage", msg =>
-			{
-				Messages.Add(msg);
-				messageView.Children.Add(new Label { Text = msg.Message });
-			});
-
-			await hubConnection.StartAsync();
-		}
-
-		public async Task Send()
-		{
-			var msg = new ChatMessage
-			{
-				UserName = nameEntry.Text,
-				Message = messageEntry.Text,
-				UserId = userId,
-				AvatarId = avatarId
-			};
-
-			await hubConnection.SendAsync("SendMessage", msg);
-		}
+			await ViewModel.Initialise();
+        }
 	}
 }
---
+++ src/MauiChat.Maui/MauiChat.Maui/ViewModels/MainViewModel.cs
@@ -0,0 +1,110 @@
+﻿using mauichat.Shared;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MauiChat.Maui.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<ChatMessage> messages { get; set; } = new ObservableCollection<ChatMessage>();
+        public ObservableCollection<ChatMessage> Messages
+        { 
+            get
+            {
+                return messages;
+            }
+
+            set
+            {
+                messages = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private HubConnection hubConnection;
+
+        private string userId;
+
+        private int avatarId;
+
+        public string UserName { get; set; }
+        public string Message { get; set; }
+
+        public ICommand SendMessageCommand { get; set; }
+
+
+        public bool IsConnected =>
+            hubConnection.State == HubConnectionState.Connected;
+
+        public MainViewModel()
+        {
+            SendMessageCommand = new Command(async () => await Send());
+            Messages = new ObservableCollection<ChatMessage>();
+        }
+
+        public async Task Initialise()
+        {
+            userId = Guid.NewGuid().ToString();
+
+            var rnd = new Random();
+
+            avatarId = rnd.Next(1, 6);
+
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://maui-chat.azurewebsites.net/chathub")
+                .Build();
+
+            hubConnection.On<ChatMessage>("ReceiveMessage", msg =>
+            {
+                try
+                {
+                    messages.Add(msg);
+                    Console.WriteLine($"Received message {msg.Message}");
+                    Console.WriteLine($"From {msg.UserName}");
+                    Messages.Clear();
+                    Messages = messages;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to receive message");
+                    Console.WriteLine(ex.Message);
+                }
+            });
+
+            await hubConnection.StartAsync();
+        }
+
+        public async Task Send()
+        {
+            var msg = new ChatMessage
+            {
+                UserName = UserName,
+                Message = Message,
+                UserId = userId,
+                AvatarId = avatarId
+            };
+
+            Console.WriteLine($"Sending message for user {userId}");
+
+            await hubConnection.SendAsync("SendMessage", msg);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
