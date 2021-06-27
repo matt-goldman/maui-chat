0ad00bb6ee4e81922a92f0384d864b8105c2f8dd
Matt Goldman <matt.goldman@outlook.com>
2021-06-28 07:26:53 +1000

    added razor lib

---
+++ src/MauiChat.MauiBlazor/MauiChat.MauiBlazor/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+using MauiChat.MauiBlazor.Data;
+
+namespace MauiChat.MauiBlazor
+{
+	public class Startup : IStartup
+	{
+		public void Configure(IAppHostBuilder appBuilder)
+		{
+			appBuilder
+				.RegisterBlazorMauiWebView(typeof(Startup).Assembly)
+				.UseMicrosoftExtensionsServiceProviderFactory()
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				})
+				.ConfigureServices(services =>
+				{
+					services.AddBlazorWebView();
+					services.AddSingleton<WeatherForecastService>();
+				});
+		}
+	}
+}
---
+++ src/MauiChat.MauiBlazor/MauiChat.MauiBlazor/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace MauiChat.MauiBlazor
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
